ede67abf1b670463933e326f22569d1f4c4cb6f4
boaldave <daveb@codeqwik.net>
2017-08-10 10:03:01 -0600

    Added IdentityServer to ASP.Net Core Project.

--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -16,22 +16,27 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // LOH 08/09/17 - AddIdentityServer registers the IdentityServer services in DI. It also registers an in-memory store for runtime state. This is useful for development scenarios. For production scenarios you need a persistent or shared store like a database or cache for that. See the EntityFramework quickstart for more information.
+            services.AddIdentityServer()
+                .AddTemporarySigningCredential();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
+                loggerFactory.AddConsole(LogLevel.Debug);
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                loggerFactory.AddConsole(LogLevel.Debug);
+            }
+
+            // LOH 08/09/17 - AddTemporarySigningCredential extension creates temporary key material for signing tokens on every start.Again this might be useful to get started, but needs to be replaced by some persistent key material for production scenarios. See the cryptography docs for more information.
+            app.UseIdentityServer();
         }
     }
 }
