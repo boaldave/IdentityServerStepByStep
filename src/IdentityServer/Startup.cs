78e0cfa250724c8277bb8fdda99bd31c9794f84d
boaldave <daveb@codeqwik.net>
2017-08-09 19:32:33 -0600

    Restructuring Project

--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace IdentityServer4
+namespace IdentityServer
 {
     public class Startup
     {
